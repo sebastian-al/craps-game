a9803c6fd5194d8a30432592f7cc2aed3b382475
AndresDavidArcos <andres.camargo@correounivalle.edu.co>
2024-10-05 18:04:29 -0500

    Corregir sintaxis

--- src/main/java/org/example/crapsgame/model/Game.java
+++ src/main/java/org/example/crapsgame/model/Game.java
@@ -1,5 +1,8 @@
 package org.example.crapsgame.model;
 
+import java.Util.Random;
+
+
 public class Game {
     private int shootCount;
     private int shoot;
@@ -12,6 +15,10 @@ public class Game {
         this.shootCount = 0;
         this.shoot = 0;
         this.point = 0;
+        this.win = false;
+        this.lose =false;
+        this.dice1 = new Dice();
+        this.dice2 = new Dice();
     }
 
     public int getShoot() {
@@ -35,7 +42,29 @@ public class Game {
     }
 
     public int rollDices() {
+        this.dice1.roll();
+        this.dice2.roll();
+        this.shoot = this.dice1.getValue() + this.dice2.getValue();
+        this.shootCount++;
         return this.shoot;
+    }
+
+    public void play ();
+
+    if ( shoot ==7 || shoot == 11) {
+        win = true;
+    } else if ( shoot == 2 || shoot == 3 || shoot ==12 ) {
+        lose = true;
+    }else {
+        point = shoot ; // EStablecemos el punto
+        while ( !win && !lose ) {
+            shoot = rollDices();
+            if ( shoot == point) {
+                win = true;
+            }else if (shoot == 7){
+                lose =true;
+            }
+        }
     }
 
 }
